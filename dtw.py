a1666e39d076454beaa413344a995f953823e8e7
Alexander Kain <kaina@ohsu.edu>
2015-12-07 13:46:23 -0800

    generalizations, improved examples, speed-up

--- dtw.py
+++ dtw.py
@@ -1,62 +1,72 @@
 from numpy import array, zeros, argmin, inf
-from numpy.linalg import norm
 
-
-def dtw(x, y, dist=lambda x, y: norm(x - y, ord=1)):
-    """ Computes the DTW of two sequences.
+def dtw(x, y, dist):
+    """
+    Computes Dynamic Time Warping (DTW) of two sequences.
 
     :param array x: N1*M array
     :param array y: N2*M array
-    :param func dist: distance used as cost measure (default L1 norm)
+    :param func dist: distance used as cost measure
 
-    Returns the minimum distance, the accumulated cost matrix and the wrap path.
-
+    Returns the minimum distance, the cost matrix, the accumulated cost matrix, and the wrap path.
     """
-    x = array(x)
-    if len(x.shape) == 1:
-        x = x.reshape(-1, 1)
-    y = array(y)
-    if len(y.shape) == 1:
-        y = y.reshape(-1, 1)
-
     r, c = len(x), len(y)
-
-    D = zeros((r + 1, c + 1))
-    D[0, 1:] = inf
-    D[1:, 0] = inf
-
+    D0 = zeros((r + 1, c + 1))
+    D0[0, 1:] = inf
+    D0[1:, 0] = inf
+    D1 = D0[1:, 1:] # view
     for i in range(r):
         for j in range(c):
-            D[i+1, j+1] = dist(x[i], y[j])
-
+            D1[i, j] = dist(x[i], y[j])
+    C = D1.copy()
     for i in range(r):
         for j in range(c):
-            D[i+1, j+1] += min(D[i, j], D[i, j+1], D[i+1, j])
+            D1[i, j] += min(D0[i, j], D0[i, j+1], D0[i+1, j])
+    return D1[-1, -1] / sum(D1.shape), C, D1, _traceback(D1)
 
-    D = D[1:, 1:]
-
-    dist = D[-1, -1] / sum(D.shape)
-
-    return dist, D, _trackeback(D)
-
-
-def _trackeback(D):
+def _traceback(D):
     i, j = array(D.shape) - 1
     p, q = [i], [j]
-    while (i > 0 and j > 0):
+    while (i and j):
         tb = argmin((D[i-1, j-1], D[i-1, j], D[i, j-1]))
-
         if (tb == 0):
-            i = i - 1
-            j = j - 1
+            i -= 1
+            j -= 1
         elif (tb == 1):
-            i = i - 1
-        elif (tb == 2):
-            j = j - 1
-
+            i -= 1
+        else: # (tb == 2):
+            j -= 1
         p.insert(0, i)
         q.insert(0, j)
-
     p.insert(0, 0)
     q.insert(0, 0)
-    return (array(p), array(q))
+    return p, q
+
+
+if __name__ == '__main__':
+    if 0: # 1-D numeric
+        from sklearn.metrics.pairwise import manhattan_distances
+        x = [0, 0, 1, 1, 2, 4, 2, 1, 2, 0]
+        y = [1, 1, 1, 2, 2, 2, 2, 3, 2, 0]
+        dist_fun = manhattan_distances
+    elif 0: # 2-D numeric
+        from sklearn.metrics.pairwise import euclidean_distances
+        x = [[0, 0], [0, 1], [1, 1], [1, 2], [2, 2], [4, 3], [2, 3], [1, 1], [2, 2], [0, 1]]
+        y = [[1, 0], [1, 1], [1, 1], [2, 1], [2, 2], [2, 3], [2, 3], [3, 1], [1, 2], [1, 0]]
+        dist_fun = euclidean_distances
+    else: # 1-D list of strings
+        from nltk.metrics.distance import edit_distance
+        x = 'this is great too'.split()
+        y = 'this too is greatly'.split()
+        dist_fun = edit_distance
+    dist, cost, acc, path = dtw(x, y, dist_fun)
+    print('Minimum distance found:', dist)
+    from matplotlib import pyplot as plt
+    plt.imshow(cost.T, origin='lower', cmap=plt.cm.Reds, interpolation='nearest')
+    plt.plot(path[0], path[1], '-o')
+    plt.xticks(range(len(x)), x)
+    plt.yticks(range(len(y)), y)
+    plt.xlabel('x')
+    plt.ylabel('y')
+    plt.axis('tight')
+    plt.show()
