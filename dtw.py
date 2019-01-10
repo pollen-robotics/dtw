a5bee3cd6e9b69355f85fb7e750e61abe74ff3b2
0mod4 <0mod4@users.noreply.github.com>
2019-01-10 13:13:45 +0100

    added windowing and slope weighting

--- dtw.py
+++ dtw.py
@@ -1,8 +1,9 @@
-from numpy import array, zeros, argmin, inf, ndim
+from numpy import array, zeros, full, argmin, inf, ndim
 from scipy.spatial.distance import cdist
+from math import isinf
 
 
-def dtw(x, y, dist, warp=1):
+def dtw(x, y, dist, warp=1, w=inf, s=1.0):
     """
     Computes Dynamic Time Warping (DTW) of two sequences.
 
@@ -10,26 +11,40 @@ def dtw(x, y, dist, warp=1):
     :param array y: N2*M array
     :param func dist: distance used as cost measure
     :param int warp: how many shifts are computed.
+    :param int w: window size limiting the maximal distance between indices of matched entries |i,j|.
+    :param float s: weight applied on off-diagonal moves of the path. As s gets larger, the warping path is increasingly biased towards the diagonal
     Returns the minimum distance, the cost matrix, the accumulated cost matrix, and the wrap path.
     """
     assert len(x)
     assert len(y)
+    assert isinf(w) or (w>=abs(len(x)-len(y)))
+    assert s>0
     r, c = len(x), len(y)
-    D0 = zeros((r + 1, c + 1))
-    D0[0, 1:] = inf
-    D0[1:, 0] = inf
+    if not isinf(w):
+        D0 = full((r + 1, c + 1), inf)
+        for i in range(1,r+1):
+            D0[i, max(1,i-w):min(c + 1,i+w+1)] = 0
+        D0[0, 0] = 0
+    else:
+        D0 = zeros((r + 1, c + 1))
+        D0[0, 1:] = inf
+        D0[1:, 0] = inf
     D1 = D0[1:, 1:]  # view
     for i in range(r):
         for j in range(c):
-            D1[i, j] = dist(x[i], y[j])
+            if (isinf(w) or (max(0,i-w) <= j <=min(c,i+w))):
+                D1[i, j] = dist(x[i], y[j])
     C = D1.copy()
+    jrange = range(c)
     for i in range(r):
-        for j in range(c):
+        if not isinf(w):
+            jrange = range(max(0, i-w),min(c, i+w+1))
+        for j in jrange:
             min_list = [D0[i, j]]
             for k in range(1, warp + 1):
                 i_k = min(i + k, r)
                 j_k = min(j + k, c)
-                min_list += [D0[i_k, j], D0[i, j_k]]
+                min_list += [D0[i_k, j]*s, D0[i, j_k]*s]
             D1[i, j] += min(min_list)
     if len(x)==1:
         path = zeros(len(y)), range(len(y))
@@ -100,11 +115,15 @@ def _traceback(D):
 
 
 if __name__ == '__main__':
-    if 0:  # 1-D numeric
+    w = inf
+    s = 1.0
+    if 1:  # 1-D numeric
         from sklearn.metrics.pairwise import manhattan_distances
         x = [0, 0, 1, 1, 2, 4, 2, 1, 2, 0]
         y = [1, 1, 1, 2, 2, 2, 2, 3, 2, 0]
         dist_fun = manhattan_distances
+        w = 1
+        #s = 1.2
     elif 0:  # 2-D numeric
         from sklearn.metrics.pairwise import euclidean_distances
         x = [[0, 0], [0, 1], [1, 1], [1, 2], [2, 2], [4, 3], [2, 3], [1, 1], [2, 2], [0, 1]]
@@ -119,7 +138,7 @@ if __name__ == '__main__':
         # x = 'we talked about the situation'.split()
         # y = 'we talked about the situation'.split()
         dist_fun = edit_distance
-    dist, cost, acc, path = dtw(x, y, dist_fun)
+    dist, cost, acc, path = dtw(x, y, dist_fun, w=w, s=s)
 
     # Vizualize
     from matplotlib import pyplot as plt
@@ -130,5 +149,8 @@ if __name__ == '__main__':
     plt.xlabel('x')
     plt.ylabel('y')
     plt.axis('tight')
-    plt.title('Minimum distance: {}'.format(dist))
+    if isinf(w):
+        plt.title('Minimum distance: {}, slope weight: {}'.format(dist, s))
+    else:
+        plt.title('Minimum distance: {}, window widht: {}, slope weight: {}'.format(dist, w, s))
     plt.show()
