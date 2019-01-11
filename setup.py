b8af125f2bf882c7d4ab1bd620a3d6e884b4109e
Pierre Rouanet <pierre.rouanet@gmail.com>
2019-01-11 16:55:48 +0100

    Update long description for pypi.

--- setup.py
+++ setup.py
@@ -6,10 +6,19 @@ from setuptools import setup, find_packages
 version = imp.load_source('dtw.version', 'dtw/version.py')
 
 
+long_description = '''
+Dtw is a Python Module for computing Dynamic Time Warping distance. It can be used as a similarity measured between temporal sequences.
+
+More info can be found at: https://github.com/pierre-rouanet/dtw
+
+It is compatible with Python 2.7-3.6 and is distributed under the GPLv3 license.
+'''
+
 setup(name='dtw',
       version=version.version,
 
       description='Python DTW Module',
+      long_description=long_description,
       author='Pierre Rouanet',
       author_email='pierre.rouanet@gmail.com',
       url='https://github.com/pierre-rouanet/dtw',
