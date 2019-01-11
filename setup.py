a42ccec6dcec970e1780fda58198bd0defed76ec
Pierre Rouanet <pierre.rouanet@gmail.com>
2019-01-11 11:56:45 +0100

    Use find_packages in setup.

--- setup.py
+++ setup.py
@@ -1,21 +1,27 @@
 #!/usr/bin/env python
 
 import imp
-from setuptools import setup
+from setuptools import setup, find_packages
 
 version = imp.load_source('dtw.version', 'dtw/version.py')
 
 
 setup(name='dtw',
       version=version.version,
+
       description='Python DTW Module',
       author='Pierre Rouanet',
       author_email='pierre.rouanet@gmail.com',
       url='https://github.com/pierre-rouanet/dtw',
       license='GNU GENERAL PUBLIC LICENSE Version 3',
 
+      packages=find_packages(),
       install_requires=['numpy', 'scipy'],
 
-      py_modules=['dtw'],
-      test_suite='tests'
+      test_suite='tests',
+
+      classifiers=[
+          "Programming Language :: Python :: 2",
+          "Programming Language :: Python :: 3",
+          "Topic :: Scientific/Engineering", ],
       )
