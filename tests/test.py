2634b6f3dc8511d723eb9d21097d3dd293085b6c
Pierre Rouanet <pierre.rouanet@gmail.com>
2019-01-11 12:02:52 +0100

    Improve import unit test coverage.

--- tests/test.py
+++ tests/test.py
@@ -1,5 +1,6 @@
 import unittest
 import numpy as np
+
 from dtw import dtw
 
 
--- tests/test_import.py
+++ tests/test_import.py
@@ -4,6 +4,8 @@ import unittest
 class ImportTestCase(unittest.TestCase):
     def test_import(self):
         import dtw
+        from dtw import dtw as dist
+        from dtw import accelerated_dtw
 
     def test_has_version(self):
         from dtw import __version__
