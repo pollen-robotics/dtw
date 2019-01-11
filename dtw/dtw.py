288b7b013f71776007b53a59415c60fc4d8ac3ec
Pierre Rouanet <pierre.rouanet@gmail.com>
2019-01-11 14:01:48 +0100

    Flake8.

--- dtw/dtw.py
+++ dtw/dtw.py
@@ -17,13 +17,13 @@ def dtw(x, y, dist, warp=1, w=inf, s=1.0):
     """
     assert len(x)
     assert len(y)
-    assert isinf(w) or (w>=abs(len(x)-len(y)))
-    assert s>0
+    assert isinf(w) or (w >= abs(len(x) - len(y)))
+    assert s > 0
     r, c = len(x), len(y)
     if not isinf(w):
         D0 = full((r + 1, c + 1), inf)
-        for i in range(1,r+1):
-            D0[i, max(1,i-w):min(c + 1,i+w+1)] = 0
+        for i in range(1, r + 1):
+            D0[i, max(1, i - w):min(c + 1, i + w + 1)] = 0
         D0[0, 0] = 0
     else:
         D0 = zeros((r + 1, c + 1))
@@ -32,19 +32,19 @@ def dtw(x, y, dist, warp=1, w=inf, s=1.0):
     D1 = D0[1:, 1:]  # view
     for i in range(r):
         for j in range(c):
-            if (isinf(w) or (max(0,i-w) <= j <=min(c,i+w))):
+            if (isinf(w) or (max(0, i - w) <= j <= min(c, i + w))):
                 D1[i, j] = dist(x[i], y[j])
     C = D1.copy()
     jrange = range(c)
     for i in range(r):
         if not isinf(w):
-            jrange = range(max(0, i-w),min(c, i+w+1))
+            jrange = range(max(0, i - w), min(c, i + w + 1))
         for j in jrange:
             min_list = [D0[i, j]]
             for k in range(1, warp + 1):
                 i_k = min(i + k, r)
                 j_k = min(j + k, c)
-                min_list += [D0[i_k, j]*s, D0[i, j_k]*s]
+                min_list += [D0[i_k, j] * s, D0[i, j_k] * s]
             D1[i, j] += min(min_list)
     if len(x) == 1:
         path = zeros(len(y)), range(len(y))
@@ -123,7 +123,7 @@ if __name__ == '__main__':
         y = [1, 1, 1, 2, 2, 2, 2, 3, 2, 0]
         dist_fun = manhattan_distances
         w = 1
-        #s = 1.2
+        # s = 1.2
     elif 0:  # 2-D numeric
         from sklearn.metrics.pairwise import euclidean_distances
         x = [[0, 0], [0, 1], [1, 1], [1, 2], [2, 2], [4, 3], [2, 3], [1, 1], [2, 2], [0, 1]]
